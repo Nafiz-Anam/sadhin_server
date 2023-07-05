a2b9136db3c7def190a2edeb3e84b496c82ef3f6
Nafiz Anam <nafiz.anam002@gmail.com>
2023-07-06 00:32:24 +0600

    bug fixing

--- routes/UserRoute/UserRouter.js
+++ routes/UserRoute/UserRouter.js
@@ -3,7 +3,6 @@ const authController = require("../../controller/authController");
 const authValidator = require("../../utilities/validations/authValidation");
 const checkUserToken = require("../../utilities/tokenmanager/checkUserToken");
 const applyUploader = require("../../uploads/applyUploder");
-const UserController = require("../../controller/userController");
 
 router.post("/send_otp", authValidator.check_user, authController.send_otp);
 router.post("/resend_otp", authValidator.check_user, authController.resend_otp);
@@ -12,10 +11,6 @@ router.post("/set_pin", authValidator.set_pin, authController.set_pin);
 router.post("/change_pin", checkUserToken, authValidator.change_pin, authController.change_pin);
 router.post("/profile/update", checkUserToken, applyUploader, authValidator.update_profile, authController.update_profile);
 router.post("/profile/details", checkUserToken, authController.profile_details);
-
-// router.post("/login", authValidator.login, authController.login);
-// router.post("/review/add", checkUserToken, UserController.add_review);
-// router.post("/review/list", checkUserToken, UserController.list_reviews);
 
 
 module.exports = router;
